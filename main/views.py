240c1b643470711a9746299d8808faf12b73032f
oyatullohm <marziyahoni@gmail.com>
2026-01-23 20:43:54 +0500

    f

--- main/views.py
+++ main/views.py
@@ -5,34 +5,34 @@ from .models import CustomUser
 
 @api_view(['POST'])
 def email_login(request):
-    data = request.data
-    email = data.get('email')
-    firebase_token = data.get('firebase_token')
+    return Response({'error': 'This endpoint has been moved to UserViewsets.email_login method.'}, status=400)
+    # data = request.data
+    # email = data.get('email')
+    # firebase_token = data.get('firebase_token')
     
-    if not email:
-        return Response({'error': ' email shart'}, status=400)
-    try:
-        user = CustomUser.objects.get(email=email)
-        print("Mavjud foydalanuvchi:", email)
-    except CustomUser.DoesNotExist:
-        user = CustomUser.objects.create_user(
-            email=email,
-            role='client',
-            username=email,
-            firebase_token=firebase_token,
-        )
-        print("Yangi foydalanuvchi yaratildi:", email)
-    user.firebase_token = firebase_token
-    refresh = RefreshToken.for_user(user)
+    # if not email:
+    #     return Response({'error': ' email shart'}, status=400)
+    # try:
+    #     user = CustomUser.objects.get(email=email)
+    # except CustomUser.DoesNotExist:
+    #     user = CustomUser.objects.create_user(
+    #         email=email,
+    #         role='client',
+    #         username=email,
+    #         firebase_token=firebase_token,
+    #     )
 
-    return Response({
-        'refresh': str(refresh),
-        'access': str(refresh.access_token),
-        'user': {
-            'id': user.id,
-            'email': user.email,
-            'phone': user.phone,
-            'role': user.role,
-            'is_confirmation': user.is_confirmation
-        }
-    }, status=200)
+    # user.firebase_token = firebase_token
+    # refresh = RefreshToken.for_user(user)
+
+    # return Response({
+    #     'refresh': str(refresh),
+    #     'access': str(refresh.access_token),
+    #     'user': {
+    #         'id': user.id,
+    #         'email': user.email,
+    #         'phone': user.phone,
+    #         'role': user.role,
+    #         'is_confirmation': user.is_confirmation
+    #     }
+    # }, status=200)
