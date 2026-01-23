b1b3d39de515ae939279993d001d6022d6ee2ba3
oyatullohm <marziyahoni@gmail.com>
2026-01-23 21:19:18 +0500

    f

--- Admin/urls.py
+++ Admin/urls.py
@@ -1,10 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
-
+from main.views import test
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('apiv1/product/', include('product.urls')),
-    path('api/', include('main.urls'))
+    path('api/', include('main.urls')),
+    path('test', test)
   
 ]
