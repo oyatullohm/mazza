ed2f3d6c7bb1a227fead85b649c94e3c46faecf9
oyatullohm <marziyahoni@gmail.com>
2026-01-30 05:16:56 +0500

    f

--- main/views.py
+++ main/views.py
@@ -36,8 +36,7 @@ def chat_create(request):
     )
     chat_room.property_id=property
     chat_room.save()
-    
-    serializer = ChatRoomSerializer(chat_room)
+    serializer = ChatRoomSerializer(chat_room, context={'request': request})
     return Response(serializer.data, status=status.HTTP_201_CREATED if created else status.HTTP_200_OK)
 
 
